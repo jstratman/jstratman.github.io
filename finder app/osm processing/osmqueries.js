52a95fcb48546aefdc43c3db0ea37eb280cc73d0
Jeremy Stratman <jeremy@mapbox.com>
2016-02-19 23:19:08 -0800

    removed callback

--- finder app/osm processing/osmqueries.js
+++ finder app/osm processing/osmqueries.js
@@ -1,27 +1,28 @@
-
-var firstStep = function() {
 var exec = require('child_process').exec,
     child, child2;
-     var turf = require('./libs/turf.js');
+var turf = require('./libs/turf.js');
 var fs = require('fs');
+var playgroundIn;  
 
- var playgroundIn;   
+var firstStep = function() {
 
- child = exec('query-overpass queries/playground_center.ql >test.geojson', {maxBuffer: 1024 * 2500},
+ 
+
+ child = exec('query-overpass queries/playground_center.ql >geojson/playground_center.geojson', {maxBuffer: 1024 * 2500},
  function (error, stdout, stderr) {
  playgroundIn = stdout;
-     console.log(playgroundIn);
-     console.log('stderr: ' + stderr);
+    // console.log(playgroundIn);
+   //  console.log('stderr: ' + stderr);
+   console.log('playground_center complete');
      if (error !== null) {
           console.log('exec error: ' + error);
      }
+	 secondStep();
  });
 
-  child2 = exec('query-overpass queries/playground_poly.ql', {maxBuffer: 1024 * 2500},
+child = exec('query-overpass queries/playground_poly.ql >geojson/playground_poly.geojson', {maxBuffer: 1024 * 2500},
  function (error, stdout, stderr) {
- 	
-    console.log(stdout);
-     console.log('stderr: ' + stderr);
+	 console.log('playground_poly complete');
      if (error !== null) {
           console.log('exec error: ' + error);
      }
@@ -31,9 +32,11 @@ var fs = require('fs');
  // Still need to time the writing of the geojson to the execution of the parsing!
 
 var secondStep = function(){
-var centers = JSON.parse(fs.readFileSync('test.geojson', 'utf8'));
-var polys = JSON.parse(fs.readFileSync('./geojson/poly_test.geojson', 'utf8'));
 
+var playground_center = JSON.parse(fs.readFileSync('./geojson/playground_center.geojson', 'utf8'));
+//var playground = JSON.parse(fs.readFileSync('./geojson/playground_center.geojson', 'utf8'));
+var playground_poly = JSON.parse(fs.readFileSync('./geojson/playground_poly.geojson', 'utf8'));
+//console.log(playground_poly.features[0].properties);
 
 var playequipment = turf.featurecollection([]); //store for playground equipment
 var playground = turf.featurecollection([]); //store for playground centroids
@@ -44,12 +47,15 @@ var toilets = turf.featurecollection([]); //store toilets
 
 
 
-keyExists(centers, "playground", playequipment); //load all playground equipment
-exactValue(centers,"leisure","playground",playgroundIn); //load all playground centroids
-exactValue(centers,"leisure","picnic_table",picnictables); //load all playground centroids
-exactValue(centers,"tourism","picnic_site",picnicsites); //load all playground centroids
-exactValue(centers,"amenity","toilets",toilets); //load all playground centroids
-
+keyExists(playground_center, "playground", playequipment); //load all playground equipment
+//exactValue(playground_center,"leisure","playground",playgroundIn); //load all playground centroids
+//exactValue(playground_center,"leisure","picnic_table",picnictables); //load all playground centroids
+//exactValue(playground_center,"tourism","picnic_site",picnicsites); //load all playground centroids
+//exactValue(playground_center,"amenity","toilets",toilets); //load all playground centroids
+exactValue(playground_center,playground); //load all playground centroids
+exactValue(playground_center,picnictables); //load all playground centroids
+exactValue(playground_center,picnicsites); //load all playground centroids
+exactValue(playground_center,toilets); //load all playground centroids
 
 function keyExists(input, key, output){
   for(var i = 0; i < input.features.length; i++) {
@@ -57,13 +63,12 @@ function keyExists(input, key, output){
       output.features.push(input.features[i]);
   }
 }};
-  
-function exactValue(input, key, value, output){
+function exactValue(input, output){  
+//function exactValue(input, key, value, output){
   for(var i = 0; i < input.features.length; i++) {
-    if(input.features[i].properties[key] == value) {
+  //  if(input.features[i].properties[key] == value) {
       output.features.push(input.features[i]);
-     // fs.writeFile('./'+key+'.geojson', JSON.stringify(output) , 'utf-8');
-  }
+ // }
 }};
 
 //Example of how to write to file (as string):
@@ -72,22 +77,26 @@ function exactValue(input, key, value, output){
 
 //process final playgrounds
 for(var i = 0; i < playground.features.length; i++) {
+console.log("u");
 //create new geojson without properties
 playground_final.features.push(turf.center(playground.features[i]));
+/*
 //nearest toilet
 var nearestToilet = turf.nearest(playground_final.features[i], toilets);
 var distanceToilet = turf.distance(playground_final.features[i], nearestToilet);
 // set name
+*/
 playground_final.features[i].properties.name = playground.features[i].properties.name;   
 // set OSM IDs
-if(playground.features[i].properties["@id"].split('/')[0] == "way") {  
-  playground_final.features[i].properties.id = "w" + playground.features[i].properties["@id"].split('/')[1];  //set OSM ID if a way
-} else if(playground.features[i].properties["@id"].split('/')[0] == "node") {
-  playground_final.features[i].properties.id = "n" + playground.features[i].properties["@id"].split('/')[1];  //set OSM ID if a node
-} else if(playground.features[i].properties["@id"].split('/')[0] == "relation") {
-  playground_final.features[i].properties.id = "r" + playground.features[i].properties["@id"].split('/')[1];  //set OSM ID if a relation
+if(playground.features[i].id.split('/')[0] == "way") {  
+  playground_final.features[i].properties.id = "w" + playground.features[i].id.split('/')[1];  //set OSM ID if a way
+} else if(playground.features[i].id.split('/')[0] == "node") {
+  playground_final.features[i].properties.id = "n" + playground.features[i].id.split('/')[1];  //set OSM ID if a node
+} else if(playground.features[i].id.split('/')[0] == "relation") {
+  playground_final.features[i].properties.id = "r" + playground.features[i].id.split('/')[1];  //set OSM ID if a relation
 };
 // set toilets- simple distance logic right now
+/*
 if(playground.features[i].properties["toilets"] == "yes" && distanceToilet >= 0.13) {  
   playground_final.features[i].properties.toilets = "unverified";
 } else if(playground.features[i].properties["toilets"] == "no") {
@@ -99,10 +108,13 @@ if(playground.features[i].properties["toilets"] == "yes" && distanceToilet >= 0.
 } else if(distanceToilet < 0.13 && distanceToilet >= 0.06 && playground.features[i].properties["toilets"] !== "yes") {
   playground_final.features[i].properties.toilets = "unverified";
 };
+*/
 // set wheelchair access
 playground_final.features[i].properties.wheelchair = playground.features[i].properties.wheelchair;   
 };
-//fs.writeFile('./'+'finalplaygrounds'+'.geojson', JSON.stringify(playground_final) , 'utf-8');
+fs.writeFile('./'+'finalplaygrounds'+'.geojson', JSON.stringify(playground_final) , 'utf-8');
+console.log('final_playgrounds complete');
 };
 
-firstStep(secondStep); //callback test
+//firstStep(secondStep()); //callback test
+firstStep();
